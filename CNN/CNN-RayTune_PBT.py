ec4553ed7c8374e11dcb232e7fa598396705c7ad
Jan Honermann <jan.honermann@cern.ch>
2021-10-13 15:39:04 +0200

    Fixed PBT training

--- CNN/CNN-RayTune_PBT.py
+++ CNN/CNN-RayTune_PBT.py
@@ -31,7 +31,7 @@ print("Number of available CPU's: {}".format(cpu_av))
 # Set the number CPUS that should be used per trial and dataloader
 # If set to 1 number of cucurrent training networking is equal to this number
 # In case of training with GPU this will be limited to number of models training simultaneously on GPU
-# So number of CPU threads for each trial can be increased 
+# So number of CPU threads for each trial can be increased
 cpus_per_trial = 2
 gpus_per_trial = 0.32
 
@@ -59,7 +59,7 @@ class CNN(nn.Module):
             nn.Conv2d(2,10, kernel_size=1),
             nn.ReLU(),
             nn.Conv2d(10,10, kernel_size=5, padding=0),
-            nn.ReLU(),  
+            nn.ReLU(),
             nn.MaxPool2d(2),
             nn.Conv2d(10,10, kernel_size=3, padding=0),
             nn.ReLU(),
@@ -67,12 +67,12 @@ class CNN(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
-        
+
         self.flatten = nn.Flatten()
-        
+
         # Gives the number of features after the conv layer
         num_features_after_conv = func_reduce(op_mul, list(self.feature_ext(torch.rand(1, *input_dim)).shape))
-        
+
         self.dense_nn = nn.Sequential(
             nn.Linear(num_features_after_conv + num_in_features, 16),
             nn.ReLU(),
@@ -83,14 +83,14 @@ class CNN(nn.Module):
             nn.Linear(4,3),
             nn.ReLU()
         )
-        
+
     def forward(self, cluster, clusNumXYEPt):
         cluster = self.feature_ext(cluster)
         x = self.flatten(cluster)
         x = torch.cat([x, clusNumXYEPt], dim=1)
         logits = self.dense_nn(x)
         return logits
-        
+
     def forward(self, cluster, clusNumXYEPt):
         cluster = self.feature_ext(cluster)
         x = self.flatten(cluster)
@@ -112,90 +112,94 @@ def train_loop(epoch, dataloader, model, loss_fn, optimizer, device="cpu"):
         Clusters = Data[0].to(device)
         Features = cm.unsqueeze_features(Data[1])
         Labels = Data[2]
-        
+
         ClusterProperties = torch.cat([Features["ClusterE"], Features["ClusterPt"], Features["ClusterM02"]
                                       , Features["ClusterM20"], Features["ClusterDist"]], dim=1).to(device)
         #Labels = torch.cat([Labels["PartPID"], dim=1]).to(device)
         Label = Labels["PartPID"].to(device)
-        
+
         if INSTANCE_NOISE:
             Clusters = add_instance_noise(Clusters, device)
-        
+
         # zero parameter gradients
         optimizer.zero_grad()
-        
+
         # prediction and loss
         pred = model(Clusters, ClusterProperties)
         loss = loss_fn(pred, Label.long())
-        
+
         # Backpropagation
         loss.backward()
         optimizer.step()
-        
+
         running_loss += loss.item()
         epoch_steps += 1
-        
+
         if batch % 100000 == 99999:
             print("[%d, %5d] loss: %.3f" % (epoch + 1, batch + 1,
                                             running_loss / epoch_steps))
-            running_loss = 0.0          
+            running_loss = 0.0
 
 
 def val_loop(epoch, dataloader, model, loss_fn, optimizer, device="cpu"):
-    
+
     val_loss = 0.0
     val_steps = 0
     total = 0
     correct = 0
     size = len(dataloader.dataset)
-    
+
     for batch, Data in enumerate(dataloader):
         with torch.no_grad():
             Clusters = Data[0].to(device)
             Features = cm.unsqueeze_features(Data[1])
             Labels = Data[2]
             ClusterProperties = torch.cat([Features["ClusterE"], Features["ClusterPt"], Features["ClusterM02"]
-                                      , Features["ClusterM20"], Features["ClusterDist"]], dim=1).to(device)           
+                                      , Features["ClusterM20"], Features["ClusterDist"]], dim=1).to(device)
             #Labels = torch.cat([Labels["PartPID"], dim=1]).to(device)
             Label = Labels["PartPID"].to(device)
-            
+
             pred = model(Clusters, ClusterProperties)
             correct += (pred.argmax(1) == Label).type(torch.float).sum().item()
 
             loss = loss_fn(pred, Label.long())#.item()
             val_loss += loss.cpu().numpy()
             val_steps += 1
-    
-    with tune.checkpoint_dir(epoch) as checkpoint_dir:
-        _path = path.join(checkpoint_dir, "checkpoint")
-        torch.save((model.state_dict(), optimizer.state_dict()), _path)
-        
+
+    if epoch % 5 == 0:
+    	with tune.checkpoint_dir(step=epoch) as checkpoint_dir:
+        	_path = path.join(checkpoint_dir, "checkpoint")
+        	torch.save({"epoch" : epoch,
+             "model_state_dict" : model.state_dict(),
+             "optimizer_state_dict" : optimizer.state_dict(),
+             "mean_accuracy" : correct / size}, _path)
+
     tune.report(loss=(val_loss / val_steps), accuracy= correct / size)
 
 
 # ## Implement method for accuracy testing on test set
 
 def test_accuracy(model, device="cpu"):
-    
+
     dataset_test = cm.load_data_test('/media/DATA/ML-Notebooks/CNN/Data/data_test.npz')
-    
+
     dataloader_test = utils.DataLoader(
         dataset_test, batch_size=4, shuffle=False, num_workers=cpu_av-1)
-    
+
     correct = 0
     total = len(dataloader_test.dataset)
-    
+
     with torch.no_grad():
         for batch, Data in enumerate(dataloader_test):
             Clusters = Data[0].to(device)
             Features = cm.unsqueeze_features(Data[1])
             Labels = Data[2]
             ClusterProperties = torch.cat([Features["ClusterE"], Features["ClusterPt"], Features["ClusterM02"]
-                                      , Features["ClusterM20"], Features["ClusterDist"]], dim=1).to(device)            
+                                      , Features["ClusterM20"], Features["ClusterDist"]], dim=1).to(device)
             #Labels = torch.cat([Labels["PartPID"], dim=1]).to(device)
             Label = Labels["PartPID"].to(device)
-            
-            
+
+
             pred = model(Clusters, ClusterProperties)
             correct += (pred.argmax(1) == Label).type(torch.float).sum().item()
 
@@ -205,10 +209,10 @@ def test_accuracy(model, device="cpu"):
 # ## Implement training routine
 
 def train_model(config, checkpoint_dir=None):
-    
+    epoch = 0
     # load model
     model = CNN()
-    
+
     # check for avlaible resource and initialize device
     device = "cpu"
     if torch.cuda.is_available():
@@ -217,43 +221,44 @@ def train_model(config, checkpoint_dir=None):
             model = nn.DataParallel(model)
     # send model to device
     model.to(device)
-    
+
     # initialise loss function and opptimizer
     loss_fn = F.cross_entropy
     optimizer = torch.optim.Adam(model.parameters(),lr=config["lr"], weight_decay=config["wd"])
-    
+
     # check whether checkpoint is available
     if checkpoint_dir:
-        model_state, optimizer_state = torch.load(
+        checkpoint = torch.load(
             path.join(checkpoint_dir, "checkpoint"))
-        model.load_state_dict(model_state)
-        optimizer.load_state_dict(optimizer_state)
-        
+        model.load_state_dict(checkpoint["model_state_dict"])
+        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+        epoch = checkpoint["epoch"]
+
+
     # load dataset
     dataset_train = cm.load_data_train('/media/DATA/ML-Notebooks/CNN/Data/data_train.npz')
-    
+
     # split trainset in train and validation subsets
     test_abs = int(len(dataset_train) * 0.8)
     subset_train, subset_val = utils.random_split(
         dataset_train, [test_abs, len(dataset_train) - test_abs])
 
-    # get dataloaders 
+    # get dataloaders
     dataloader_train, dataloader_val = get_dataloader(subset_train, subset_val, int(config["batch_size"]))
-                                                      
-    for epoch in range(100):
+
+    while True:
         train_loop(epoch, dataloader_train, model, loss_fn, optimizer, device=device)
-        val_loop(epoch, dataloader_train, model, loss_fn, optimizer, device=device)                                              
-    
-    print("Finished Training")
+        val_loop(epoch, dataloader_train, model, loss_fn, optimizer, device=device)
+        epoch += 1
 
 
 # ## Setup all Ray Tune functionality and start training
 
 def main(num_samples=10, max_num_epochs=10, gpus_per_trial=1):
-    
+
     # Setup hyperparameter-space to search
     config = {
-        "lr": tune.loguniform(1e-4, 1e-1),
+        "lr": tune.loguniform(1e-4, 1e1),
         "wd" : tune.uniform(0, 1e-1),
         "batch_size": tune.choice([16, 32, 64, 128, 256])
     }
@@ -263,35 +268,52 @@ def main(num_samples=10, max_num_epochs=10, gpus_per_trial=1):
     time_attr="training_iteration",
     metric="accuracy",
     mode="max",
-    perturbation_interval=10)
-        
-    
+    perturbation_interval=5)
+
+
     # Init the Reporter
     reporter = CLIReporter(
         parameter_columns=["lr", "wd", "batch_size"],
         metric_columns=["loss", "accuracy", "training_iteration"])
-    
+
     #Get Current date and time
     timestr = strftime("%Y_%m_%d-%H:%M:%S")
-    name = "ASHA-" + timestr
-    
+    name = "PBT-" + timestr
+
+    class CustomStopper(tune.Stopper):
+        def __init__(self):
+            self.should_stop = False
+
+        def __call__(self, trial_id, result):
+            if not self.should_stop and result["mean_accuracy"] > 0.9:
+                self.should_stop = True
+            return self.should_stop or result["training_iteration"] >= max_num_epochs
+
+        def stop_all(self):
+            return self.should_stop
+
+    stopper = CustomStopper()
+
     # Init the run method
     result = tune.run(
-        func_partial(train_model),
+        train_model,
         name = name,
         resources_per_trial={"cpu": cpus_per_trial, "gpu": gpus_per_trial},
         config=config,
         num_samples=num_samples,
         local_dir = "./Ray_Results",
         scheduler=scheduler,
-        progress_reporter=reporter)
-    
+        stop=stopper,
+        #progress_reporter=reporter,
+        checkpoint_score_attr="mean_accuracy",
+        keep_checkpoints_num=4)
+
     # Find best trial and use it on the testset
     best_trial = result.get_best_trial("loss", "min", "last")
     print("Best trial config: {}".format(best_trial.config))
     print("Best trial final validation loss: {}".format(best_trial.last_result["loss"]))
     print("Best trial final validation accuracy: {}".format(best_trial.last_result["accuracy"]))
-    
+
     best_trained_model = CNN()
     device = "cpu"
     if torch.cuda.is_available():
@@ -299,12 +321,12 @@ def main(num_samples=10, max_num_epochs=10, gpus_per_trial=1):
         if gpus_per_trial > 1:
             best_trained_model = nn.DataParallel(best_trained_model)
     best_trained_model.to(device)
-    
+
     best_checkpoint_dir = best_trial.checkpoint.value
     model_state, optimizer_state = torch.load(path.join(
         best_checkpoint_dir, "checkpoint"))
     best_trained_model.load_state_dict(model_state)
-    
+
     test_acc = test_accuracy(best_trained_model, device)
     print("Best trial test set accuracy: {}".format(test_acc))
 
@@ -313,10 +335,3 @@ def main(num_samples=10, max_num_epochs=10, gpus_per_trial=1):
 
 if __name__ == "__main__":
 	main(num_samples=10, max_num_epochs=100, gpus_per_trial=gpus_per_trial)
-
-
-
-
-
-
-
